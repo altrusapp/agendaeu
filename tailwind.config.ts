a06ca21763e419af71e32ae6f20c3fe544fe5b81
Claudinei Felipe <claudinei.felip@gmail.com>
2025-08-28 02:21:18 +0000

    Realize um refinamento visual completo do aplicativo com foco em moderni

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,9 +74,10 @@ export default {
         },
       },
       borderRadius: {
+        xl: 'calc(var(--radius) + 4px)',
         lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        md: 'calc(var(--radius) - 4px)',
+        sm: 'calc(var(--radius) - 8px)',
       },
       keyframes: {
         'accordion-down': {
