e78ac0c030b59ab673ca096ec4f3ec94737db398
Claudinei Felipe <claudinei.felip@gmail.com>
2025-09-13 03:33:44 +0000

    Analise o seguinte erro, pois não estou conseguindo fazer deploy:
    [00:2

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase/client.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -12,8 +13,8 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// Initialize Firebase App (Lazy Initialization)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
